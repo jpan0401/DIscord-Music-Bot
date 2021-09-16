2a2ad444c79db6cfdc062151b40782141f9572ab
Joseph <jpan0401@gmail.com>
2021-09-16 11:51:53 -0400

    adding files

---
+++ main.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    startup();
+});
+
+function startup(){
+    console.log('Shucklebot is online!');
+
+}
+
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if(command === 'clear') {
+        client.commands.get('clear').execute(message, args);
+    } else if (command === 'play') {
+        client.commands.get('play').execute(message, args);
+    } else if (command === 'leave') {
+        client.commands.get('leave').execute(message, args);
+    }
+});
+
+client.login('ODg3Nzg0NzM5OTQ2MjQ2MjI1.YUJL-A.s2Lyr7rl-rUD9PK0TyEXXsQGHn0');
